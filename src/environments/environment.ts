f97c2e547ba2faef3bf5b976ffb9eea20c811293
Aly Mamdouh <aly.mamdouh@interactts.com>
2024-12-21 10:47:05 +0200

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   // baseUrl: 'https://localhost:52665/api/',
   baseUrl: 'https://oasisofttaskbackend-cqgxdnaxewgzfban.eastus-01.azurewebsites.net/api/',
   //redirectUrl: 'http://localhost:4200/',
-  redirectUrl: 'https://proud-wave-017cdc60f.5.azurestaticapps.net/',
+  redirectUrl: 'https://oasisofttaskfrontend.alysoftwaredeveloper.net/',
 };
